3ee18cd6828474f13ad9d8876a173d59e3af47ba
Adao Natalino <sansonov@gmail.com>
2020-04-05 14:19:47 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -10,12 +10,10 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+ 
   final_cart = []
   cart.each do |hash|
-       binding.pry
-    
+    if cart[]
     
   end
     
