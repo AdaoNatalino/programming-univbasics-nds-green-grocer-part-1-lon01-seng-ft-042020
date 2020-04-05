909a318e7755a3eb42812b5e406c55be864fda75
Adao Natalino <sansonov@gmail.com>
2020-04-05 15:53:06 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -14,13 +14,14 @@ def consolidate_cart(cart)
   cart.each do |item_hash|
     found_item = find_item_by_name_in_collection(item_hash[:item], cart)
     if found_item
-      final_cart.each do |final_cart_item|
-        if final_cart_item[:item] == found_item[:item]
-          final_cart_item[:count] +=1
-          else
-            final_cart_item[:count] = 1
-        end
-      end
+      
+      # final_cart.each do |final_cart_item|
+      #   if final_cart_item[:item] == found_item[:item]
+      #     final_cart_item[:count] +=1
+      #     else
+      #       final_cart_item[:count] = 1
+      #   end
+      # end
     end
   end
   return final_cart
