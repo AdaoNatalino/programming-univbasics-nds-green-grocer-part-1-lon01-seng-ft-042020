0597a3c5bc20b594d6ec2dbb390a0dd95701e7fe
Adao Natalino <sansonov@gmail.com>
2020-04-05 13:38:26 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,10 +1,10 @@
 require 'pry'
 def find_item_by_name_in_collection(name, collection)
   collection.each do |hash|
-  #binding.pry
-  if hash[:item] === name
-  return hash
-  end
+    #binding.pry
+    if hash[:item] === name
+    return hash
+    end
   end
 end
 
